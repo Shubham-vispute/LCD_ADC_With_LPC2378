f35aa0960ec24aeb3ae178108277549e21e8e574
Shubham Vispute <shubhwithu7@gmail.com>
2024-10-21 21:22:44 +0530

    initial commit

---
+++ lcd_adc_2024ht01026.d
@@ -0,0 +1,7 @@
+.\lcd_adc_2024ht01026.o: LCD_ADC_2024ht01026.c
+.\lcd_adc_2024ht01026.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\lcd_adc_2024ht01026.o: lcd.h
+.\lcd_adc_2024ht01026.o: C:\Keil\ARM\Inc\Philips\lpc23xx.h
+.\lcd_adc_2024ht01026.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\lcd_adc_2024ht01026.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\lcd_adc_2024ht01026.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
